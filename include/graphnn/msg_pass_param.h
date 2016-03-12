4c38a45f9a85dcd161ecc083ab8727f22b5cc1ef
hanjun <daihanjun@gmail.com>
2016-03-11 19:07:19 -0500

    done concat

--- include/graphnn/msg_pass_param.h
+++ include/graphnn/msg_pass_param.h
@@ -97,4 +97,53 @@ protected:
         virtual void InitCPUWeight(GraphStruct* graph) override;
 };
 
+template<MatMode mode, typename Dtype>
+class SubgraphConcatParam : public IConstParam<mode, Dtype>
+{
+public:
+		SubgraphConcatParam(std::string _name) 
+            : IConstParam<mode, Dtype>(_name), graph(nullptr) {}
+
+        virtual void ResetOutput(const IMatrix<mode, Dtype>* input, DenseMat<mode, Dtype>* output) override
+        {
+            assert(graph->num_nodes % graph->num_subgraph == 0);
+            int node_per_graph = graph->num_nodes / graph->num_subgraph; 
+            // assume the labels are consecutive
+            for (int i = 0; i < node_per_graph; ++i)
+                for (int j = 0; j < graph->num_subgraph; ++j)
+                    assert(graph->subgraph->head[j][i] == j * node_per_graph + i); 
+            
+            output->Zeros(graph->num_subgraph, graph->num_nodes / graph->num_subgraph * input->cols);
+        }
+             				 		
+		virtual void UpdateOutput(IMatrix<mode, Dtype>* input, DenseMat<mode, Dtype>* output, Dtype beta, Phase phase) override
+        {
+            assert(beta == 0);
+            int row = output->rows, col = output->cols;            
+            output->CopyFrom(input);
+            // resize will not change the content in output
+            output->Resize(row, col);             
+        }
+        
+        virtual void UpdateGradInput(DenseMat<mode, Dtype>* gradInput, DenseMat<mode, Dtype>* gradOutput, Dtype beta) override
+        {
+            assert(gradInput->rows * gradInput->cols == gradOutput->rows * gradOutput->cols);
+            gradOutput->Resize(gradInput->rows, gradInput->cols);
+            
+            if (beta == 0)
+                gradInput->CopyFrom(*gradOutput);
+            else
+                gradInput->Axpby(1.0, *gradOutput, beta);
+        }
+        
+        virtual void InitConst(void* side_info) override
+        {
+            this->graph = static_cast<GraphStruct*>(side_info);
+        }
+
+protected:
+        GraphStruct* graph;         
+};
+
+
 #endif
