72ab8d0a4e0f5f19495ff8c66e45503c8d75ab2a
hanjun <daihanjun@gmail.com>
2016-03-17 19:03:03 -0400

    done cpu

--- include/graphnn/graph_pool_param.h
+++ include/graphnn/graph_pool_param.h
@@ -24,10 +24,24 @@ class NodeMaxPoolParam<CPU, Dtype> : public IConstParam<CPU, Dtype>
 {
 public:
         NodeMaxPoolParam(std::string _name)
-            : IConstParam<CPU, Dtype>(_name) {}
-            
+            : IConstParam<CPU, Dtype>(_name) { max_index.clear(); }
+
+        virtual void InitConst(void* side_info) override
+        {
+            graph = static_cast<GraphStruct*>(side_info); 
+        }
+        		
+        virtual void ResetOutput(const IMatrix<CPU, Dtype>* input, DenseMat<CPU, Dtype>* output) override
+        {
+            output->Zeros(graph->num_nodes, input->cols);
+        }
+             				 		
+		virtual void UpdateOutput(IMatrix<CPU, Dtype>* input, DenseMat<CPU, Dtype>* output, Dtype beta, Phase phase) override;
+        
+        virtual void UpdateGradInput(DenseMat<CPU, Dtype>* gradInput, DenseMat<CPU, Dtype>* gradOutput, Dtype beta) override;                     
 protected:
-                
+        GraphStruct* graph;
+        std::vector<int> max_index;                
 };
 
 template<typename Dtype>
--- src/graphnn/graph_pool_param.cpp
+++ src/graphnn/graph_pool_param.cpp
@@ -37,3 +37,50 @@ template class NodeAvgPoolParam<GPU, double>;
 template class NodeAvgPoolParam<GPU, float>;
 
 // =============================== NodeMaxPoolParam =========================================
+
+template<typename Dtype>
+void NodeMaxPoolParam<CPU, Dtype>::UpdateOutput(IMatrix<CPU, Dtype>* input, DenseMat<CPU, Dtype>* output, Dtype beta, Phase phase)
+{
+        if (max_index.size() < output->count)
+            max_index.resize(output->count);
+        Dtype* input_feature = input->DenseDerived().data;
+        size_t cur_offset, neighbor_offset;
+        for (size_t i = 0; i < graph->num_nodes; ++i)
+        {
+            auto& list = graph->in_edges->head[i];
+            for (size_t j = 0; j < output->cols; ++j)
+            {
+                cur_offset = i * input->cols + j;
+                Dtype cur_best = input_feature[cur_offset];
+                max_index[cur_offset] = cur_offset;
+                for (size_t k = 0; k < list.size(); ++k)
+                {
+                    neighbor_offset = list[k].second * input->cols + j;
+                    if (input_feature[neighbor_offset] > cur_best)
+                    {
+                        cur_best = input_feature[neighbor_offset];
+                        max_index[cur_offset] = neighbor_offset;
+                    }                    
+                }
+                output->data[cur_offset] = output->data[cur_offset] * beta + cur_best;
+            }            
+        }
+}
+
+template<typename Dtype>
+void NodeMaxPoolParam<CPU, Dtype>::UpdateGradInput(DenseMat<CPU, Dtype>* gradInput, DenseMat<CPU, Dtype>* gradOutput, Dtype beta)
+{
+        auto& prev_grad = gradInput->DenseDerived();
+        
+        prev_grad.Scale(beta);
+        size_t offset;
+        for (size_t i = 0; i < prev_grad.rows; ++i)
+            for (size_t j = 0; j < prev_grad.cols; ++j)
+            {
+                offset = i * prev_grad.cols + j;
+                prev_grad.data[max_index[offset]] += gradOutput->data[offset];
+            }
+}
+
+template class NodeMaxPoolParam<CPU, double>;
+template class NodeMaxPoolParam<CPU, float>;
