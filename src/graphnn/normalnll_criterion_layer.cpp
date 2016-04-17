e2883a4de617cf336197749b3baa9cb151536b2d
Hanjun-Dai <daihanjun@gmail.com>
2016-04-17 14:55:20 -0400

    added normal nll

---
+++ src/graphnn/normalnll_criterion_layer.cpp
@@ -0,0 +1,67 @@
+#include "normalnll_criterion_layer.h"
+#include "dense_matrix.h"
+
+#define pi 3.14159265358979
+
+template<MatMode mode, typename Dtype>
+NormalNLLCriterionLayer<mode, Dtype>::NormalNLLCriterionLayer(std::string _name, Dtype _lambda, PropErr _properr)
+        : ICriterionLayer<mode, Dtype>(_name, _lambda, _properr)
+{
+    this->grad = new DenseMat<mode, Dtype>();
+    this->state = new DenseMat<mode, Dtype>();    
+}        
+        
+template<MatMode mode, typename Dtype>
+void NormalNLLCriterionLayer<mode, Dtype>::UpdateOutput(std::vector< ILayer<mode, Dtype>* >& operands, Phase phase)
+{
+    assert(operands.size() == 3);
+    auto& mu = operands[0]->state->DenseDerived();
+    auto& sigma = operands[1]->state->DenseDerived();
+    auto& x = operands[2]->state->DenseDerived();
+    
+    // (x - mu)^2
+    auto& buf = this->state->DenseDerived(); 
+    buf.GeaM(1.0, Trans::N, mu, -1.0, Trans::N, x);
+    buf.Square();
+    
+    // (x - mu)^2 / 2 / sigma / sigma
+    buf.Scale(0.5);
+    buf.EleWiseDiv(sigma);
+    buf.EleWiseDiv(sigma);
+    
+    // (x - mu)^2 / 2 / sigma / sigma + 0.5 * log(2 pi)    
+    buf.Add(0.5 * log(2 * pi));
+    
+    // log(sigma) + (x - mu)^2 / 2 / sigma / sigma + 0.5 * log(2 pi)
+    auto& grad = this->grad->DenseDerived();
+    grad.Log(sigma);
+    buf.Axpy(1.0, grad);
+        
+    this->loss = buf.Sum();
+    
+    if (this->properr == PropErr::T)
+        grad.GeaM(1.0, Trans::N, mu, -1.0, Trans::N, x);
+}
+
+template<MatMode mode, typename Dtype>
+void NormalNLLCriterionLayer<mode, Dtype>::BackPropErr(std::vector< ILayer<mode, Dtype>* >& operands, unsigned cur_idx, Dtype beta)
+{
+    assert(operands.size() == 3 && cur_idx < 3);
+    
+    auto& diff = this->grad->DenseDerived();
+    auto& prev_grad = operands[cur_idx]->grad->DenseDerived();
+    
+    // d/d_mu
+    if (cur_idx == 0)
+    {
+            
+    } else // d/d_sigma
+    {
+        
+    }
+}
+
+template class NormalNLLCriterionLayer<CPU, float>;
+template class NormalNLLCriterionLayer<CPU, double>;
+template class NormalNLLCriterionLayer<GPU, float>;
+template class NormalNLLCriterionLayer<GPU, double>;
