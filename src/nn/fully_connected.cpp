aab226146926d188937817fd7e24104312a06e43
Hanjun-Dai <daihanjun@gmail.com>
2017-02-17 00:12:02 -0500

    allow sparse

--- src/nn/fully_connected.cpp
+++ src/nn/fully_connected.cpp
@@ -19,22 +19,24 @@ void FullyConnected<mode, Dtype>::Forward(std::vector< std::shared_ptr<Variable>
 
 	auto& output = dynamic_cast<DTensorVar<mode, Dtype>*>(outputs[0].get())->value;
 
-	auto& param = dynamic_cast<DTensorVar<mode, Dtype>*>(operands[1].get())->value;
-	auto& input = dynamic_cast<DTensorVar<mode, Dtype>*>(operands[0].get())->value;
+	auto& param = dynamic_cast<DTensorVar<mode, Dtype>*>(operands[1].get())->value;	
 
-	auto weight = param.GetRowRef(0, input.cols());
+	auto* lhs = dynamic_cast< TensorVar<mode, Dtype>* >(operands[0].get());
+	MAT_TYPE_SWITCH(lhs->GetMatType(), matType, {
+		auto& input = lhs->template Derived<matType>().value;
+		auto weight = param.GetRowRef(0, input.cols());
 
-	output.MM(input, weight, Trans::N, Trans::N, 1.0, 0.0);
+		output.MM(input, weight, Trans::N, Trans::N, 1.0, 0.0);
+		if (param.rows() > input.cols())
+		{
+			ASSERT(param.rows() == input.cols() + 1, "size mismatch");
+			auto bias = param.GetRowRef(input.cols(), 1);
 
-	if (param.rows() > input.cols())
-	{
-		ASSERT(param.rows() == input.cols() + 1, "size mismatch");
-		auto bias = param.GetRowRef(input.cols(), 1);
-
-		auto mul = DTensor<mode, Dtype>({input.rows(), (size_t)1});
-		mul.Fill(1.0);
-		output.MM(mul, bias, Trans::N, Trans::N, 1.0, 1.0);
-	}
+			auto mul = DTensor<mode, Dtype>({input.rows(), (size_t)1});
+			mul.Fill(1.0);
+			output.MM(mul, bias, Trans::N, Trans::N, 1.0, 1.0);
+		}
+	});
 }
 
 template<typename mode, typename Dtype>
@@ -50,29 +52,31 @@ void FullyConnected<mode, Dtype>::Backward(std::vector< std::shared_ptr<Variable
 	auto* rhs = dynamic_cast<DTensorVar<mode, Dtype>*>(operands[1].get());
 	auto& param_grad = rhs->grad;
 
-	auto* lhs = dynamic_cast<DTensorVar<mode, Dtype>*>(operands[0].get());
+	auto* lhs = dynamic_cast< TensorVar<mode, Dtype>* >(operands[0].get());
 
 	if (!isConst[1])
 	{
-		auto& input = lhs->value;
+		MAT_TYPE_SWITCH(lhs->GetMatType(), matType, {
+			auto& input = lhs->template Derived<matType>().value;
+			auto weight_grad = param_grad.GetRowRef(0, input.cols());		
 
-		auto weight_grad = param_grad.GetRowRef(0, input.cols());		
+			weight_grad.MM(input, grad_out, Trans::T, Trans::N, 1.0, 1.0);
+			if (param_grad.rows() > input.cols())
+			{
+				auto bias_grad = param_grad.GetRowRef(input.cols(), 1);
 
-		weight_grad.MM(input, grad_out, Trans::T, Trans::N, 1.0, 1.0);
-		if (param_grad.rows() > input.cols())
-		{
-			auto bias_grad = param_grad.GetRowRef(input.cols(), 1);
+				auto mul = DTensor<mode, Dtype>({input.rows(), (size_t)1});
+				mul.Fill(1.0);
 
-			auto mul = DTensor<mode, Dtype>({input.rows(), (size_t)1});
-			mul.Fill(1.0);
-
-			bias_grad.MM(mul, grad_out, Trans::T, Trans::N, 1.0, 1.0);
-		}
+				bias_grad.MM(mul, grad_out, Trans::T, Trans::N, 1.0, 1.0);
+			}
+		});	
 	}
 
 	if (!isConst[0])
 	{
-		auto& input_grad = lhs->grad;
+		ASSERT(lhs->GetMatType() == MatType::dense, "differentiable lhs can't be sparse");
+		auto& input_grad = lhs->template Derived<DENSE>().grad;
 		auto weight = rhs->value.GetRowRef(0, input_grad.cols());
 
 		input_grad.MM(grad_out, weight, Trans::N, Trans::T, 1.0, 1.0);
